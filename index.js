bf5c51659cfcbdfcf89ef45d44f79548e3e25c33
Christopher Marzulli <chriszulli15@gmail.com>
2022-05-10 16:04:05 -0400

    commit tree f6b575309e486f26f7f80cf6dd372db8f948a804

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch("https://anapioficeandfire.com/api/books")
+  .then((resp) => resp.json())
+  .then((json) => renderBooks(json))
 }
 
 function renderBooks(books) {
